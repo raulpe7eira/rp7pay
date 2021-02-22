d854e859d73bd749523854be05d5c874cd296af7
Raul Pereira <raul.pereira@gmail.com>
2021-02-22 17:33:23 -0300

    edit feature: sum_from_file w/ stream

--- lib/rp7pay/numbers.ex
+++ lib/rp7pay/numbers.ex
@@ -10,7 +10,7 @@ defmodule Rp7pay.Numbers do
       result
       |> String.trim
       |> String.split(",")
-      |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Stream.map(fn number -> String.to_integer(number) end)
       |> Enum.sum
 
     {:ok, %{result: result}}
