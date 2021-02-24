fe178b06a884bca41ce6953d2a4b915bbda74f13
Raul Pereira <raul.pereira@gmail.com>
2021-02-24 16:10:42 -0300

    add migration for account

--- lib/rp7pay_web/views/users_view.ex
+++ lib/rp7pay_web/views/users_view.ex
@@ -1,13 +1,17 @@
 defmodule Rp7payWeb.UsersView do
   alias Rp7pay.User
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname, account: account}}) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account.id,
+          balance: account.balance
+        }
       }
     }
   end
