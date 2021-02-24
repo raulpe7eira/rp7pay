70f6b7feaf83449dcd7756a017fbde540855bd8a
Raul Pereira <raul.pereira@gmail.com>
2021-02-24 15:15:51 -0300

    add fallback_controller

--- lib/rp7pay_web/controllers/users_controller.ex
+++ lib/rp7pay_web/controllers/users_controller.ex
@@ -2,23 +2,14 @@ defmodule Rp7payWeb.UsersController do
   use Rp7payWeb, :controller
 
   alias Rp7pay.User
-  alias Ecto.Changeset
+
+  action_fallback Rp7payWeb.FallbackController
 
   def create(conn, params) do
-    params
-    |> Rp7pay.create_user
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-  defp handle_response({:error, %Changeset{} = changeset}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(Rp7payWeb.ErrorView)
-    |> render("400.json", changeset: changeset)
+    with {:ok, %User{} = user} <- Rp7pay.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 end
