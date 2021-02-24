34dca88099e24b58668dc39f903babac54ba38b2
Raul Pereira <raul.pereira@gmail.com>
2021-02-24 18:27:50 -0300

    add feature deposit + withdraw w/o view

--- lib/rp7pay.ex
+++ lib/rp7pay.ex
@@ -1,5 +1,11 @@
 defmodule Rp7pay do
   alias Rp7pay.Users.Create, as: UserCreate
 
+  alias Rp7pay.Accounts.Deposit, as: AccountDeposit
+  alias Rp7pay.Accounts.Withdraw, as: AccountWithdraw
+
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit_account(params), to: AccountDeposit, as: :call
+  defdelegate withdraw_account(params), to: AccountWithdraw, as: :call
 end
--- lib/rp7pay/users/create.ex
+++ lib/rp7pay/users/create.ex
@@ -4,23 +4,21 @@ defmodule Rp7pay.Users.Create do
 
   def call(params) do
     Multi.new
-    |> Multi.insert(:create_user, User.changeset(params))
-    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
-      insert_account(repo, user)
-    end)
-    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
-      preload_data(repo, user)
-    end)
+    |> Multi.insert(:create_user, changeset_user(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user) end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} -> preload_data(repo, user) end)
     |> run_transaction
   end
 
+  defp changeset_user(params), do: User.changeset(params)
+
   defp insert_account(repo, user) do
     user.id
-    |> account_changeset
+    |> changeset_account
     |> repo.insert
   end
 
-  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
+  defp changeset_account(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
 
   defp preload_data(repo, user), do: {:ok, repo.preload(user, :account)}
 
