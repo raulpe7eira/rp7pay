c593fbec6785358b60d7d2997afa49d251e6f8b5
Raul Pereira <raul.pereira@gmail.com>
2021-02-24 15:04:07 -0300

    improves error view

--- lib/rp7pay_web/views/error_view.ex
+++ lib/rp7pay_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule Rp7payWeb.ErrorView do
   use Rp7payWeb, :view
 
+  alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -12,5 +14,17 @@ defmodule Rp7payWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{changeset: %Changeset{} = changeset}) do
+    %{errors: %{detail: translate_erros(changeset)}}
+  end
+
+  defp translate_erros(changeset) do
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
