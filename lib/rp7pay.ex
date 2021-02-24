873827511f41f135a607de64a9d32ca6c0ba27fb
Raul Pereira <raul.pereira@gmail.com>
2021-02-23 22:51:21 -0300

    add migration for user

--- lib/rp7pay.ex
+++ lib/rp7pay.ex
@@ -1,9 +1,5 @@
 defmodule Rp7pay do
-  @moduledoc """
-  Rp7pay keeps the contexts that define your domain
-  and business logic.
+  alias Rp7pay.Users.Create, as: UserCreate
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_user(params), to: UserCreate, as: :call
 end
---
+++ lib/rp7pay_web/views/users_view.ex
@@ -0,0 +1,14 @@
+defmodule Rp7payWeb.UsersView do
+  alias Rp7pay.User
+
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+    %{
+      message: "User created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname
+      }
+    }
+  end
+end
